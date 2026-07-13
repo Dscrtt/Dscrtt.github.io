a5fc9c08ef52a20bb43a1de7a269ff90fa4c3bc3
Zul Akhyar <zakhyar38@gmail.com>
2026-07-13 19:13:19 +0700

    update

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
+const body = document.body;
+//default mode
+body.setAttribute('mode', 'light');
+
 function Mode() {
-  const body = document.body;
-  if (body.getAttribute('mode') === 'dark') {
-      document.querySelector('.mode-btn').textContent = '🏙️';
-      body.setAttribute('mode', 'light');
-  } else {
-      document.querySelector('.mode-btn').textContent = '🌃';
-      body.setAttribute('mode', 'dark');
-  }
+    if (body.getAttribute('mode') === 'dark') {
+        document.querySelector('.mode-btn').textContent = '🏙️';
+        body.setAttribute('mode', 'light');
+    } else {
+        document.querySelector('.mode-btn').textContent = '🌃';
+        body.setAttribute('mode', 'dark');
+    }
 }
